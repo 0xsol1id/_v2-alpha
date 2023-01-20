f6e26d2b8d8a1aae8e09a7abb2c9a330694b661a
0xsol1id <0xsol1d@soljunks.io>
2023-01-20 17:46:07 +0100

    origin/main

--- src/views/MintView/MintButton.tsx
+++ src/views/MintView/MintButton.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import Button from '@material-ui/core/Button';
-import { CircularProgress } from '@material-ui/core';
-import { CandyMachine } from './candy-machine';
+import {CircularProgress} from '@material-ui/core';
+import {GatewayStatus, useGateway} from '@civic/solana-gateway-react';
+import {CandyMachine} from './candy-machine'; 
 
 
 export const CTAButton = styled(Button)`
@@ -14,13 +15,13 @@ export const CTAButton = styled(Button)`
 `;
 
 export const MintButton = ({
-    onMint,
-    candyMachine,
-    isMinting,
-    isEnded,
-    isActive,
-    isSoldOut
-}: {
+                               onMint,
+                               candyMachine,
+                               isMinting,
+                               isEnded,
+                               isActive,
+                               isSoldOut
+                           }: {
     onMint: (quantityString: number) => Promise<void>;
     candyMachine: CandyMachine | undefined;
     isMinting: boolean;
@@ -28,16 +29,21 @@ export const MintButton = ({
     isActive: boolean;
     isSoldOut: boolean;
 }) => {
+    const {requestGatewayToken, gatewayStatus} = useGateway();
     const [clicked, setClicked] = useState(false);
     const [isVerifying, setIsVerifying] = useState(false);
 
     useEffect(() => {
         setIsVerifying(false);
-        if (clicked) {
+        if (gatewayStatus === GatewayStatus.COLLECTING_USER_INFORMATION && clicked) {
             // when user approves wallet verification txn
             setIsVerifying(true);
+        } else if (gatewayStatus === GatewayStatus.ACTIVE && clicked) {
+            console.log('Verified human, now minting...');
+            onMint(1);
+            setClicked(false);
         }
-    }, [clicked, setClicked, onMint]);
+    }, [gatewayStatus, clicked, setClicked, onMint]);
 
     return (
         <CTAButton
@@ -51,8 +57,14 @@ export const MintButton = ({
                 isVerifying
             }
             onClick={async () => {
-                console.log('Minting...');
-                await onMint(1);
+                if (isActive && candyMachine?.state.gatekeeper && gatewayStatus !== GatewayStatus.ACTIVE) {
+                    console.log('Requesting gateway token');
+                    setClicked(true);
+                    await requestGatewayToken();
+                } else {
+                    console.log('Minting...');
+                    await onMint(1);
+                }
             }}
             variant="contained"
         >
@@ -63,7 +75,7 @@ export const MintButton = ({
             ) : isActive ? (
                 isVerifying ? 'VERIFYING...' :
                     isMinting || clicked ? (
-                        <CircularProgress />
+                        <CircularProgress/>
                     ) : (
                         "MINT"
                     )
